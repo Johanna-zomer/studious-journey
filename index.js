d6dc5d2691272c2200587bed22fdc4dbe9804a19
Johanna-zomer <84843160+Johanna-zomer@users.noreply.github.com>
2021-06-21 14:12:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+let name = "Hannie";
+console.log(name);
+//if kan gebruikt worden bij een block an codes als een bepaalde waarde true is
+console.log(4 + 4);
+console.log("4" + "4")//string optellen gaat niet, string wordt als text ervaren
+console.log(20 + 3);
+console.log(30 - 10);
+console.log(3 * 4);
+console.log(15 / 3);
+console.log(typeof 60);
+console.log(typeof "zestig")
+
