b9f3278a833a111766bf4a6dfd188e83d5fdf451
Johanna-zomer <84843160+Johanna-zomer@users.noreply.github.com>
2021-06-23 15:16:57 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+console.log();
+const myAge = 16;
+if (myAge > 17) {
+    document.write ("Welkom, je mag naar binnen");
+} else {
+    document.write ("Sorry, helaas kunnen we je niet toelaten");
+}
+const isFemale = true;
+if (isFemale){
+    document.write ("Welkom op Ladiesnight");
+} else {
+    document.write ("Sorry, dit is een ladies only avond");
+}
+const driverStatus = bob;
+if (driverStatus == bob) {
+    document.write ("Goede reis naar huis");
+} else {
+    document.write ("We zullen een taxi voor je bellen");
+}
