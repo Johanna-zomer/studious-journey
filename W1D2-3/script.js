00b99d9941e8de89edb387a6e8bfd8eb79ec8ec3
Johanna-zomer <84843160+Johanna-zomer@users.noreply.github.com>
2021-06-23 11:21:53 +0200

    Add files via upload

---
+++ W1D2-3/script.js
@@ -0,0 +1,10 @@
+console.log ("Hallo Winc Academy studenten");
+//Dit is een grote som
+let multiplication = 1230942 * 1823794;
+console.log ("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log ("division:", division);
+/* 
+let multiplication = 1230942 * 1823794;
+let division = 637263 / 54;
+*/
