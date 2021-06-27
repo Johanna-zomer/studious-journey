fe84c05ec53c8385ca4807f703fe3ab6b0cf425d
Johanna-zomer <84843160+Johanna-zomer@users.noreply.github.com>
2021-06-27 16:18:58 +0200

    Add files via upload

---
+++ javascriptF.js
@@ -0,0 +1,17 @@
+console.log();
+
+const makePizza = function () {
+    console.log ("kneed het meel met het water");
+    console.log ("rol het deeg uit");
+    console.log ("smeer er de tomatensaus over");
+    console.log ("bedek met de topping naar keuze");
+    console.log ("zet 20 minuten in de oven op 200 graden");
+};
+makePizza();
+
+const makeSushi = function () {
+    console.log ("pak maar wat");
+    console.log ("rol het in elkaar");
+    console.log ("geef er stokjes bij");
+};
+makeSushi();
