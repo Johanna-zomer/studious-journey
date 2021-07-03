d02f1111753d23267975675f70401a2491dd7bd3
Johanna-zomer <84843160+Johanna-zomer@users.noreply.github.com>
2021-07-03 13:00:36 +0200

    Add files via upload

---
+++ iterators.js
@@ -0,0 +1,20 @@
+const arrayColors = ['yellow', 'blue', 'red', 'orange'];
+
+// arrayColors.forEach(element => console.log(element));
+
+//while loop
+let n = arrayColors;
+
+while (n <= 4){
+    n++;
+}
+// console.log (n);
+
+//For loop
+let n = arrayColors;
+
+for (let i=0; i<=4; i++); {
+ 
+}
+console.log (n);
+
