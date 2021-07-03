f2739c4390b8e18a55c0e5a0e4b14e825f3cfcb8
Johanna-zomer <84843160+Johanna-zomer@users.noreply.github.com>
2021-07-03 12:04:32 +0200

    Add files via upload

---
+++ Javascripts Loops/even-odd-reporter.js
@@ -0,0 +1,12 @@
+for (let i = 0; i <= 20; i++){ 
+  if (i%2 !=0) {
+  continue;
+    
+    }
+     
+  console.log(`Is ${i} even`);
+ 
+  
+    // TODO: determine if i is odd or even
+    // TODO: use console.log
+  }
